a0450dee1731b5ae3018a29692624b5e2f8b18e8
Wosty <36647710+Wosty@users.noreply.github.com>
2018-10-01 00:12:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,23 @@ import os
 import sys
 import json
 import time
-
-#from groupy.client import Client
+import random
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-
 from flask import Flask, request
-
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
   log('Recieved {}'.format(data))
-
-  if '?' in data['text']:
-    #msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    time.sleep(2)
-    msg = 'Read the syllabus.'
-    send_message(msg)
+  faceList = open('faces.txt','r').readlines()
+  msg = random.choice(faceList).strip('\n')
+  send_message(msg)
   return "ok", 200
 
 def send_message(msg):
   url  = 'https://api.groupme.com/v3/bots/post'
-
   data = {
           'bot_id' : os.getenv('GROUPME_BOT_ID'),
           'text'   : msg,
